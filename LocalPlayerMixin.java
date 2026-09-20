fff28e3ef505bdb39e474d0ede8a53fbbab50192
Jonathan Hillman <jonathandalehillman@gmail.com>
2026-09-19 20:05:08 -0500

    Update LocalPlayerMixin.java

--- LocalPlayerMixin.java
+++ LocalPlayerMixin.java
@@ -1,32 +1,33 @@
 package com.jonathan.nocreativedrift.mixin;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LocalPlayer.class)
+@Mixin(ClientPlayerEntity.class)
 public abstract class LocalPlayerMixin {
     @Inject(method = "tick", at = @At("TAIL"))
     private void noCreativeFlightDrift(CallbackInfo ci) {
-        LocalPlayer player = (LocalPlayer) (Object) this;
-        Minecraft minecraft = Minecraft.getInstance();
+        ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
+        MinecraftClient client = MinecraftClient.getInstance();
 
-        if (!player.getAbilities().flying || minecraft.options == null) {
+        if (!player.getAbilities().flying || client.options == null) {
             return;
         }
 
-        boolean movingHorizontally = minecraft.options.keyUp.isDown()
-                || minecraft.options.keyDown.isDown()
-                || minecraft.options.keyLeft.isDown()
-                || minecraft.options.keyRight.isDown();
+        boolean movingHorizontally =
+                client.options.forwardKey.isPressed()
+                || client.options.backKey.isPressed()
+                || client.options.leftKey.isPressed()
+                || client.options.rightKey.isPressed();
 
         if (!movingHorizontally) {
-            Vec3 velocity = player.getDeltaMovement();
-            player.setDeltaMovement(0.0D, velocity.y, 0.0D);
+            Vec3d velocity = player.getVelocity();
+            player.setVelocity(0.0D, velocity.y, 0.0D);
         }
     }
 }
